4c3082e5dadc2bf67d0ddc86be77d9fc8b507a0d
jairius519 <155593713+jairius519@users.noreply.github.com>
2026-06-06 20:20:21 -0500

    Update User.java

--- User.java
+++ User.java
@@ -3,14 +3,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.persistence.*;
 
 @Entity //An Entity is a Java class that maps to a database table
-//User is a reserved word in SQL, so we need to use the @table annotation and pass in "users"
-@Table(name = "users") //specifies the name of the database table that this entity maps to
+@Table(name = "users") //the name of the database table
 public class User {
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long userID; //a unique id designed for users that cannot be changed 
-   private String name;//the name of the user 
+   private Long userID; //a unique id designed for users 
+   private String name; //the name of the user 
    private String email; //the email of the user
    private String password; //the password of the user
    private int age; // the age of the user
@@ -18,13 +17,13 @@ public class User {
    public User() {
     }
 
-    public User (String name, String email, String password, int age) {
-    this.name = name;
+   public User (String name, String email, String password, int age) {
+     this.name = name;
      this.email = email; 
      this.password = password; 
      this.age = age;
      }
-//sets the name of the user
+
 public void setName(String name) {
     this.name = name;
 }
@@ -32,7 +31,6 @@ public void setEmail(String email) {
     this.email = email;
 }
 
-//sets the password of the user
 public void setPassword(String password) {
     this.password = password;
 }
@@ -40,21 +38,19 @@ public void setPassword(String password) {
 public void setAge(int age) {
     this.age = age;
 }
-//returns the user's identification
+
 public Long getUserID() {
     return userID;
 }
-//returns the user's name 
+
 public String getName() {
     return name;
 }
 
-//returns the user's email address
 public String getEmail() {
     return email;
 }
 
-//returns the user's password
 public String getPassword() {
     return password;
 }
